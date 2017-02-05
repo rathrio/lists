2503dc7530bdd49ffb35fef6997acb74c439c8da
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-02-05 22:31:14 +0100

    Add list scaffold for testing

---
+++ app/models/list.rb
@@ -0,0 +1,5 @@
+class List < ApplicationRecord
+  def to_s
+    name
+  end
+end
---
+++ db/migrate/20170205212344_create_lists.rb
@@ -0,0 +1,9 @@
+class CreateLists < ActiveRecord::Migration[5.0]
+  def change
+    create_table :lists do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
