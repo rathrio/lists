40dc50730e8efec35d2ec0e3d61195bdae899fc4
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-02-05 21:30:01 +0100

    Place socket in folder that pi user has ownership to

--- config/puma.rb
+++ config/puma.rb
@@ -43,7 +43,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 # end
 
-bind 'unix:///var/run/lists.sock'
+bind 'unix:///var/run/puma/lists.sock'
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
