537a7f9bd60d765d41f5879c12b7ba8ba544c66a
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-02-07 23:29:49 +0100

    Allow items to be updated from scraper results

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,13 @@ class Item < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
+  validates :name, presence: true
+
   def self.create_from(scraper_result)
-    create!(scraper_result)
+    create!(scraper_result.merge(scraped: true))
+  end
+
+  def update_from(scraper_result)
+    update_attributes!(scraper_result.merge(scraped: true))
   end
 end
