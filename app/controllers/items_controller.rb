280deb022037c39f1fda7d853ab5752ba45f2363
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-02-08 14:00:55 +0100

    View prototype

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,18 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.all.reverse_order
+  end
+
+  def create
+    item = Item.new item_params
+    result = MovieScraper.new(query: item.name).scrape.first
+    item.update_from(result)
+    redirect_to action: :index
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name)
   end
 end
