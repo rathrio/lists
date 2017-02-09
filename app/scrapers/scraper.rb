11816be87cac6e88cd9c1d39fd2044f69b8c4058
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-02-09 20:15:04 +0100

    Add scrape template method

--- app/scrapers/scraper.rb
+++ app/scrapers/scraper.rb
@@ -14,6 +14,43 @@ module Scraper
   end
 
   def scrape
+    search_results.map do |result|
+      {
+        name: scrape_name(result),
+        description: scrape_description(result),
+        remote_image_url: scrape_image(result),
+        date: scrape_date(result),
+        links: scrape_links(result),
+        tags: scrape_tags(result)
+      }
+    end
+  end
+
+  def search_results
+    raise NotImplementedError
+  end
+
+  def scrape_name(result)
+    raise NotImplementedError
+  end
+
+  def scrape_description(result)
+    raise NotImplementedError
+  end
+
+  def scrape_image(result)
+    raise NotImplementedError
+  end
+
+  def scrape_date(result)
+    raise NotImplementedError
+  end
+
+  def scrape_links(result)
+    raise NotImplementedError
+  end
+
+  def scrape_tags(result)
     raise NotImplementedError
   end
 end
