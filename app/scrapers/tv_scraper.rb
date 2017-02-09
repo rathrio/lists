cb93939798ce5546bf0014277fbea3ff29dfb40c
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-02-09 08:12:35 +0100

    Correctly scrape tv show years

--- app/scrapers/tv_scraper.rb
+++ app/scrapers/tv_scraper.rb
@@ -4,24 +4,24 @@ class TvScraper
   end
 
   def scrape
-    movies = MoviedbClient.new(query: @query).search(type: :tv)['results'].to_a
+    shows = MoviedbClient.new(query: @query).search(type: :tv)['results'].to_a
 
-    movies.map do |movie|
-      url = if movie['poster_path']
-              MoviedbClient::IMAGE_BASE_URI + movie['poster_path']
+    shows.map do |show|
+      url = if show['poster_path']
+              MoviedbClient::IMAGE_BASE_URI + show['poster_path']
             else
               ''
             end
 
-      date = if movie['release_date'].present?
-               Date.parse(movie['release_date'])
+      date = if show['first_air_date'].present?
+               Date.parse(show['first_air_date'])
              else
                nil
              end
 
       {
-        name: movie['name'],
-        description: movie['overview'],
+        name: show['name'],
+        description: show['overview'],
         remote_image_url: url,
         date: date
       }
