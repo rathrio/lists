b4a848b4970cf4ff1bfcc92940ab22db81402e0c
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-02-14 23:05:47 +0100

    Add youtube link to item box

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def set_label_ids(ids)
     session[:label_ids] = ids
   end
+
+  def youtube_search_url(query)
+    "https://www.youtube.com/results?search_query=#{URI.escape(query)}"
+  end
 end
