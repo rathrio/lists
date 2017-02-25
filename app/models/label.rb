dd85f65683ae811e4d1e846021a26ab7c9759731
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-02-25 21:35:43 +0100

    Create default labels for a new user

--- app/models/label.rb
+++ app/models/label.rb
@@ -5,7 +5,10 @@ class Label < ApplicationRecord
   validates :name, presence: true
 
   def self.create_defaults(user)
-    
+    user.labels.find_or_create_by(name: 'Movies', scraper: 'MovieScraper', fa_icon: 'film')
+    user.labels.find_or_create_by(name: 'TV', scraper: 'TvScraper', fa_icon: 'television')
+    user.labels.find_or_create_by(name: 'Music', scraper: 'AlbumScraper', fa_icon: 'music')
+    user.labels.find_or_create_by(name: 'Games', scraper: 'GameScraper', fa_icon: 'gamepad')
   end
 
   def default_scraper
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
   has_many :tags, dependent: :destroy
 
   validates :email, :encrypted_password, presence: true
+  validates :email, uniqueness: true
+
+  after_create :create_default_labels
+
+  private
+
+  def create_default_labels
+    Label.create_defaults(self)
+  end
 end
