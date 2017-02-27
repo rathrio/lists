36a9f82d605b03cfadc08f0c6cf4782f79ed2e9a
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-02-27 08:33:48 +0100

    Add tag form

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -10,10 +10,25 @@ class TagsController < ApplicationController
   end
 
   def edit
+    @tag = current_user.tags.find(params[:id])
+    render 'form'
   end
 
   def update
+    @tag = current_user.tags.find(params[:id])
+
+    if @tag.update_attributes(tag_params)
+      flash[:notice] = 'Tag successfully updated'
+      redirect_to edit_tag_path(@tag)
+    else
+      flash[:alert] = 'Could not update Tag'
+      render 'form'
+    end
   end
 
   private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
 end
