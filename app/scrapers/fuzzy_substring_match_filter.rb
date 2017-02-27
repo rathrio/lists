d0e1e0413837f5860953a92d487dcde4c305954e
simplay <silent.simplay@gmail.com>
2017-02-27 22:16:09 +0100

    Improved documentation, naming and test coverage

--- app/scrapers/fuzzy_substring_match_filter.rb
+++ app/scrapers/fuzzy_substring_match_filter.rb
@@ -1,15 +1,22 @@
 # Forward only results that exhibit a substring match with
-# the used query word.
+# a given query word.
+#
+# This filter is used to reduce the number of false positive results provided
+# by our scappers and is used within the ScraperResultsController class.
 class FuzzySubstringMatchFilter
 
   attr_reader :matches
 
+  # Defines how many errors are tolerated to accept a string comparison
+  # as a good match.
   TOLERATED_ERRORS = 0
+
+  # Delimiters are ignored for determining the matching quality
   DELIMITERS = [
     '-',
     '/',
     ' '
-  ]
+  ].freeze
 
   # @param result [Array<Hash> #name] retrieved results
   # @param query [String] user search word
@@ -17,33 +24,58 @@ class FuzzySubstringMatchFilter
     @matches = results.select do |result|
       result_name = normalized_word(result[:name])
       query_name = normalized_word(query)
-      distance_between_close_enough?(result_name, query_name)
+      words_are_similar_enough?(result_name, query_name)
     end
   end
 
-  def distance_between_close_enough?(word, substring)
-    n = substring.length 
-    splits = (split_word word).select { |word| word.length == n }
-    splits.any? do |s|
-      n - overlapping_chararcters(s, substring) == TOLERATED_ERRORS
+  # Tests whether there is a substring of a given result,
+  # which has sufficient many overlapping characters.
+  #
+  # @example
+  #   words_are_similar_enough?("foobarbaz", "bar")
+  #   #=> true
+  #   words_are_similar_enough?("hanspeter", "nils")
+  #   #=> false
+  #
+  # @param result [String] a normalized result fetched by a scapper.
+  # @param query [String] a normalized query word provided by the user.
+  # @return [Boolean] true if there is a substring in the result, that has
+  #   sufficient many matches with the given query word, otherwise false.
+  def words_are_similar_enough?(result, query)
+    n = query.length
+    substrings_of_length_n = (substrings_for result).select { |substring| substring.length == n }
+    substrings_of_length_n.any? do |substring|
+      number_of_missmatches = n - overlapping_character_count(substring, query)
+      number_of_missmatches <= TOLERATED_ERRORS
     end
   end
 
-  def overlapping_chararcters(s, substring)
-    sum = 0
-    n = s.length
-    n.times do |idx|
-      sum += 1 if (s[idx].eql? substring[idx])
+  # Count the number of overlapping characters for
+  # two given equally sized words.
+  #
+  # @example
+  #   overlapping_character_count("foobaz", "foobar")
+  #   #=> 5
+  #
+  # @param word_a [String] some word
+  # @param word_b [String] some word
+  # @return [Integer] number of overlapping characters between
+  #   the two given words.
+  def overlapping_character_count(word_a, word_b)
+    overlap_count = 0
+    word_length = word_a.length
+    word_length.times do |idx|
+      overlap_count += 1 if word_a[idx].eql?(word_b[idx])
     end
-    sum
+    overlap_count
   end
 
   # Down-cases and removes all delimiter tokens from a given word.
-  # 
+  #
   # @example
   #   normalized_word("FooBar-Pew)
   #   #=> "foobarpew"
-  #   
+  #
   # @param word [String] unnormalized word
   # @return [String] downcased, delimiter-less word.
   def normalized_word(word)
@@ -54,17 +86,18 @@ class FuzzySubstringMatchFilter
     tmp
   end
 
-  # Obtain all substring of a given word.
-  # 
+  # Obtain all substring combinations of a given word.
+  #
   # @example
-  #   split_word("abc")
+  #   substrings_for("abc")
   #   #=> ["a", "ab", "abc", "b", "bc", "c"]
-  # 
+  #
   # @param s [String] word to split
   # @return [Array<String>] collection of substrings of a given word.
-  def split_word(s)
-    indices = (0...s.length).to_a
-    indices.product(indices).reject{|i,j| i > j}.map{|i,j| s[i..j]}.uniq
+  def substrings_for(word)
+    indices = (0...word.length).to_a
+    upper_triangle = indices.product(indices).reject { |i, j| i > j }
+    upper_triangle.map { |i, j| word[i..j] }.uniq
   end
 
 end
--- test/scrapers/fuzzy_substring_match_filter_test.rb
+++ test/scrapers/fuzzy_substring_match_filter_test.rb
@@ -4,10 +4,10 @@ class FuzzySubstringMatchFilterTest < ActiveSupport::TestCase
 
   test 'Only matches partial substrings' do
     results = [
-      {name: "He Man: Vol 1"},
-      {name: "The He Man: Vol 1"},
-      {name: "Her Men: Vol 1"},
-      {name: "Simpons - He is the real man"}
+      { name: "He Man: Vol 1" },
+      { name: "The He Man: Vol 1" },
+      { name: "Her Men: Vol 1" },
+      { name: "Simpons - He is the real man" }
     ]
 
     gt = [
@@ -17,7 +17,7 @@ class FuzzySubstringMatchFilterTest < ActiveSupport::TestCase
 
     matches = FuzzySubstringMatchFilter.new(results, "He-Man").matches
     matches.each do |match|
-      assert(gt.include? match[:name])
+      assert gt.include?(match[:name])
     end
   end
 
@@ -25,8 +25,8 @@ class FuzzySubstringMatchFilterTest < ActiveSupport::TestCase
     fm = FuzzySubstringMatchFilter
     gt = "He Man: Vol 1"
     results = [
-      {name: gt},
-      {name: "Simpons - He is the real man"}
+      { name: gt },
+      { name: "Simpons - He is the real man" }
     ]
 
     assert_equal(gt, fm.new(results, "He-Man").matches[0][:name])
@@ -37,12 +37,34 @@ class FuzzySubstringMatchFilterTest < ActiveSupport::TestCase
     assert_equal(gt, fm.new(results, "He-mAn").matches[0][:name])
   end
 
+  test '#word_are_similar_enough?' do
+    fm = FuzzySubstringMatchFilter.new([], '')
+    assert fm.words_are_similar_enough?("foo", "foo")
+    assert fm.words_are_similar_enough?("foobar", "foo")
+    assert fm.words_are_similar_enough?("pewfoobar", "foo")
+    refute fm.words_are_similar_enough?("pewfoobar", "pow")
+    refute fm.words_are_similar_enough?("", "pow")
+    refute fm.words_are_similar_enough?("Pow", "pow")
+    refute fm.words_are_similar_enough?("hanspeter", "nils")
+    assert fm.words_are_similar_enough?("hanspeter", "hans")
+    assert fm.words_are_similar_enough?("hanspeter", "peter")
+  end
+
+  test '#overlapping_chararcters' do
+    fm = FuzzySubstringMatchFilter.new([], '')
+    assert_equal(0, fm.overlapping_character_count("", ""))
+    assert_equal(3, fm.overlapping_character_count("foo", "foo"))
+    assert_equal(0, fm.overlapping_character_count("Otto", "Nils"))
+    assert_equal(5, fm.overlapping_character_count("foobar", "foobaz"))
+    assert_equal(5, fm.overlapping_character_count("foobaz", "foobar"))
+  end
+
   test 'Filter does not consider DELIMITERS' do
     fm = FuzzySubstringMatchFilter
     gt = "He Man: Vol 1"
     results = [
-      {name: gt},
-      {name: "Simpons - He is the real man"}
+      { name: gt },
+      { name: "Simpons - He is the real man" }
     ]
 
     fm::DELIMITERS.each do |delimiter|
@@ -59,19 +81,17 @@ class FuzzySubstringMatchFilterTest < ActiveSupport::TestCase
     assert_equal("foobar", fm.normalized_word("FOO/BAR"))
   end
 
-  test "#split_word" do
+  test "#substrings_for" do
     fm = FuzzySubstringMatchFilter.new([], '')
-    gt_splits = [
-      "a", "ab", "abc", "b", "bc", "c"
-    ]
+    gt_splits = %w(a ab abc b bc c)
 
-    splits = fm.split_word("abc")
-    splits.each do |split|
-      assert(gt_splits.include? split)
+    substrings = fm.substrings_for("abc")
+    substrings.each do |split|
+      assert gt_splits.include?(split)
     end
 
     gt_splits.each do |split|
-      assert(splits.include? split)
+      assert substrings.include?(split)
     end
   end
 
