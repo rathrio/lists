2b0d92ced91bd412c0ab9b200ac46636a63208cf
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-02-28 18:36:25 +0100

    Add examples to scraper doc

--- app/scrapers/scraper.rb
+++ app/scrapers/scraper.rb
@@ -1,4 +1,35 @@
-# Include this module to a class to make it behave like a scraper.
+# Include this module make a class behave like a scraper.
+#
+# There are a bunch of template methods you can override to tweak the scraping
+# behaviour:
+#
+#   * #search_results: an array of results, most likely from a remote API call
+#   * #scrape_<attribute>(result): extracts <attribute> from one result
+#
+# @example Example scraper definition
+#
+#   # in app/scrapers/action_movie_scraper.rb
+#
+#   class ActionMovieScraper
+#     include Scraper
+#
+#     def search_results
+#       action_movie_api.search(query) # => e.g. [{ title: 'foo' }, { title: 'bar' }]
+#     end
+#
+#     def scrape_name(result)
+#       result[:title]
+#     end
+#   end
+#
+# @example Update an item with the first result of ActionMovieScraper results
+#
+#   scraper = ActionMovieScraper.new(query: 'lethal weapon')
+#   result = scraper.scrape.first # => { name: 'lethal weapon 2: reloaded' }
+#
+#   item = Item.create # ...
+#   item.update_from(result)
+#   item.name # => 'lethal weapon 2: reloaded'
 module Scraper
 
   # @return [Array<Scraper>] list of classes that include this module.
@@ -38,10 +69,25 @@ module Scraper
   #   Each element will get passed to the scraper template methods in order to
   #   extract the desired data.
   def search_results
-    []
+    raise NotImplementedError, <<~EOS
+      Implement #search_results and make it return an array or results.
+    EOS
   end
 
   def scrape_name(result)
+    raise NotImplementedError, <<~EOS
+      Implement #scrape_name(result) and extract the value from result that
+      correspond with the name of an item.
+
+      result is one element of whatever #search_results returns.
+
+      Example:
+
+      # Assuming #search_results returns an array of hashes with key :title
+      def scrape_name(result)
+        result[:title]
+      end
+    EOS
   end
 
   def scrape_description(result)
