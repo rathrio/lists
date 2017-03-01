097e4b0408379324c91610e64b962e58b3853cfa
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-03-01 08:39:28 +0100

    Add import action to results controller

--- app/controllers/scraper_results_controller.rb
+++ app/controllers/scraper_results_controller.rb
@@ -7,4 +7,22 @@ class ScraperResultsController < ApplicationController
     results = results.map { |r| OpenStruct.new(r) }
     render partial: 'scraper_results', locals: { results: results }
   end
+
+  def import
+    Item.create_from(result_params, user: current_user)
+    redirect_to items_path(label_ids: current_label_id_params)
+  end
+
+  private
+
+  def result_params
+    params.require(:scraper_results).permit(
+      :name,
+      :description,
+      :remote_image_url,
+      :date,
+      :links,
+      :tags
+    )
+  end
 end
