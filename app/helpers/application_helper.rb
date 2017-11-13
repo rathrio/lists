eaa87f035862c703e6d83de54c7daa93379cc3ae
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-11-13 20:51:20 +0100

    Add pirate link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def youtube_search_url(query)
     "https://www.youtube.com/results?search_query=#{URI.escape(query)}"
   end
+
+  def pirate_search_url(query)
+    "https://thepiratebay.org/search/#{URI.escape(query)}"
+  end
 end
