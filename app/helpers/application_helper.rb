a2a38498dd0a19870432582d184ae83652f2f149
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-11-13 21:11:27 +0100

    More links!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def pirate_search_url(query)
     "https://thepiratebay.org/search/#{URI.escape(query)}"
   end
+
+  def google_search_url(query)
+    "https://www.google.ch/search?q=#{URI.escape(query)}"
+  end
+
+  def netflix_search_url(query)
+    "https://www.netflix.com/search?q=#{URI.escape(query)}"
+  end
 end
