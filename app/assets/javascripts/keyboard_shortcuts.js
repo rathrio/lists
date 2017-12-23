c746ad9efb306ff8bfbbe9dbd8d4a68aced02e37
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-12-23 22:03:28 +0100

    Add 1-9 list shortcuts

--- app/assets/javascripts/keyboard_shortcuts.js
+++ app/assets/javascripts/keyboard_shortcuts.js
@@ -1,5 +1,6 @@
 (function() {
   function focusFilter() {
+    window.scrollTo(0, 0);
     $('.filter').select();
     return false;
   }
@@ -16,10 +17,27 @@
     visit('/items?archived=true');
   }
 
+  function gotoList(n) {
+    var key = n.key;
+    var selector = '[data-shortcut="' + key + '"]';
+    var link = $(selector);
+
+    if (link.length === 0) {
+      return;
+    }
+
+    var url = link.attr("href");
+    visit(url);
+  }
+
   function ready() {
     Mousetrap.bind('/', focusFilter);
     Mousetrap.bind('g h', gotoHome);
     Mousetrap.bind('g a', gotoArchived);
+
+    [1, 2, 3, 4, 5, 6, 7, 8, 9].forEach(function(n) {
+      Mousetrap.bind(n.toString(), gotoList);
+    });
   }
 
   $(document).on('turbolinks:load', ready);
