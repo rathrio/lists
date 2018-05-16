9a88b8ab57ca9f2096930280297df3d34738febc
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-05-17 00:55:39 +0200

    Remove hello world bundle

--- app/javascript/packs/lists-bundle.js
+++ app/javascript/packs/lists-bundle.js
@@ -1,10 +1,8 @@
 import ReactOnRails from 'react-on-rails';
 
-import HelloWorld from '../bundles/Lists/components/HelloWorld';
 import ItemsList from '../bundles/Lists/components/ItemsList';
 
-// This is how react_on_rails can see the HelloWorld in the browser.
+// This is how react_on_rails can see the ItemsList in the browser.
 ReactOnRails.register({
-  // HelloWorld,
   ItemsList
 });
