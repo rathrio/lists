8a8aaa7684eabfea835311b4883c3903b72bd16f
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-05-26 17:31:04 +0200

    Port restore and really_destroy actions

--- app/javascript/bundles/Lists/components/ItemBox.jsx
+++ app/javascript/bundles/Lists/components/ItemBox.jsx
@@ -1,17 +1,54 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 
-const ItemBox = ({ item, onTagClick, onArchive }) => {
+const ArchiveActions = ({ onArchiveClick }) => (
+  <div className="level-item">
+    <a target="blank" href="#" onClick={onArchiveClick}>
+      <span className="icon is-medium" data-balloon="Archive"><i className="fa fa-archive fa-lg"></i></span>
+    </a>
+  </div>
+)
+
+const RestoreActions = ({ onRestoreClick, onDeleteClick }) => (
+  <Fragment>
+    <div className="level-item">
+      <a target="blank" href="#" onClick={onRestoreClick}>
+        <span className="icon is-medium" data-balloon="Restore"><i className="fa fa-recycle fa-lg"></i></span>
+      </a>
+    </div>
+    <div className="level-item">
+      <a target="blank" href="#" onClick={onDeleteClick}>
+        <span className="icon is-medium" data-balloon="Delete for good"><i className="fa fa-trash fa-lg"></i></span>
+      </a>
+    </div>
+  </Fragment>
+)
+
+const ItemBox = ({ item, onTagClick, onArchive, onRestore, onDelete }) => {
   const thumbUrl = item.image.thumb.url
+
+  const pirateSearchUrl = encodeURI(`https://thepiratebay.org/search/${item.name}`)
+  const youtubeSearchUrl = encodeURI(`https://www.youtube.com/results?search_query=${item.name}`)
+  const googleSearchUrl = encodeURI(`https://www.google.ch/search?q=${item.name}`)
+  const netflixSearchUrl = encodeURI(`https://www.netflix.com/search?q=${item.name}`)
 
   const onArchiveClick = (e) => {
     e.preventDefault()
     onArchive(item)
   }
 
-  const pirateSearchUrl = encodeURI(`https://thepiratebay.org/search/${item.name}`)
-  const youtubeSearchUrl = encodeURI(`https://www.youtube.com/results?search_query=${item.name}`)
-  const googleSearchUrl = encodeURI(`https://www.google.ch/search?q=${item.name}`)
-  const netflixSearchUrl = encodeURI(`https://www.netflix.com/search?q=${item.name}`)
+  const onRestoreClick = (e) => {
+    e.preventDefault()
+    onRestore(item)
+  }
+
+  const onDeleteClick = (e) => {
+    e.preventDefault()
+    onDelete(item)
+  }
+
+  const itemActions = (item.deleted) ?
+    <RestoreActions onRestoreClick={onRestoreClick} onDeleteClick={onDeleteClick} /> :
+    <ArchiveActions onArchiveClick={onArchiveClick} />
 
   return (
     <div className="box item-box">
@@ -27,11 +64,21 @@ const ItemBox = ({ item, onTagClick, onArchive }) => {
             <div className="subtitle is-4"><a href={`/items/${item.id}`}>{item.name}</a></div>
           </div>
 
-          <div className="level-item has-pointer" onClick={() => onTagClick(item.year)} data-balloon={`Show ${item.year} items`}>
-            <span className="tag is-rounded is-light is-small">
-              {item.year}
-            </span>
-          </div>
+          {item.year && (
+            <div className="level-item has-pointer" onClick={() => onTagClick(item.year)} data-balloon={`Show ${item.year} items`}>
+              <span className="tag is-rounded is-light is-small">
+                {item.year}
+              </span>
+            </div>
+          )}
+
+          {item.deleted && (
+            <div className="level-item has-pointer">
+              <span className="tag is-rounded is-light is-small">
+                {item.list}
+              </span>
+            </div>
+          )}
 
           {item.tags.map((tag) => {
             return (
@@ -69,11 +116,7 @@ const ItemBox = ({ item, onTagClick, onArchive }) => {
             </a>
           </div>
 
-          <div className="level-item has-pointer">
-            <a target="blank" href="#" onClick={onArchiveClick}>
-              <span className="icon is-medium" data-balloon="Archive"><i className="fa fa-archive fa-lg"></i></span>
-            </a>
-          </div>
+          {itemActions}
         </div>
       </div>
     </div>
--- app/javascript/bundles/Lists/components/ItemList.jsx
+++ app/javascript/bundles/Lists/components/ItemList.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import ItemBox from './ItemBox'
 
-const ItemList = ({ items, onTagClick, onItemArchive }) => {
+const ItemList = ({ items, onTagClick, onItemArchive, onItemRestore, onItemDelete }) => {
   const itemBoxes = items.map((i) => (
     <ItemBox
       key={`item-${i.id}`}
       item={i}
       onTagClick={onTagClick}
-      onArchive={onItemArchive} />
+      onArchive={onItemArchive}
+      onRestore={onItemRestore}
+      onDelete={onItemDelete} />
   ))
 
   return (
