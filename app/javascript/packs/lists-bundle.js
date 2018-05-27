78bf57f30da31845da342a8b6c5afd5d58718bfc
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-05-27 15:00:32 +0200

    Move mousetrap to webpack

--- app/javascript/packs/lists-bundle.js
+++ app/javascript/packs/lists-bundle.js
@@ -1,4 +1,5 @@
 import ReactOnRails from 'react-on-rails';
+import '../bundles/utils/keyboard_shortcuts';
 import Items from '../bundles/Lists/components/Items';
 
 // This is how react_on_rails can see the Items component in the browser.
