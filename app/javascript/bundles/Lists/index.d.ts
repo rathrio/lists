50af89e6f35c98a3228e57c0b9dffd8d90d43827
Radi <rad.iyadurai@gmail.com>
2018-06-29 00:11:00 +0200

    Typescript (#55)
    TypeScript Port

---
+++ app/javascript/bundles/Lists/index.d.ts
@@ -0,0 +1,36 @@
+export interface Image {
+  thumb: { url: string };
+  url: string;
+}
+
+export interface Item {
+  id: number;
+  list: string;
+  list_id: number;
+  name: string;
+  description: string;
+  quantity: number;
+  scraped: boolean;
+  status: string;
+  human_status: string;
+  tags: string[];
+  user_id: number;
+  year: number;
+  rating?: number;
+  created_at: string;
+  updated_at: string;
+  deleted: boolean;
+  deleted_at: string;
+  date: string;
+  image?: Image;
+  links?: string[];
+}
+
+export interface ScraperResult {
+  name: string;
+  description: string;
+  remote_image_url: string;
+  date: string;
+  tags: string[];
+  links?: string[];
+}
---
+++ app/javascript/bundles/utils/rails.js
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+const Rails = {
+  authenticityToken: ReactOnRails.authenticityToken,
+  visit: Turbolinks.visit
+};
+
+export default Rails;
