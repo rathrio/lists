800902a1069b74a2f3c75aee885b9977b7504aa0
Radi <rad.iyadurai@gmail.com>
2018-06-30 18:47:27 +0200

    More include/exclude ts settings

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,7 +2,7 @@ const { environment } = require('@rails/webpacker')
 
 environment.loaders.append('typescript', {
   test: /\.tsx?$/,
-  exclude: /node_modules|public/,
+  exclude: /node_modules/,
   loader: 'awesome-typescript-loader'
 });
 
