3bddcd954e1b1d4da6ba7303ce493343fe9f1320
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-08-14 11:27:02 +0200

    Update dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -88,7 +88,6 @@ ActiveRecord::Schema.define(version: 2018_05_29_142709) do
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
-    t.string "api_token"
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
