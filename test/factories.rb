c356f358d57d974ad13eb05ccc185ede9d64edc1
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-08-24 16:42:57 +0200

    Fix factory bot warnings

--- test/factories.rb
+++ test/factories.rb
@@ -1,22 +1,22 @@
 FactoryBot.define do
   factory :item do
-    name FFaker::Movie.title
+    name { FFaker::Movie.title }
     user
     list
   end
 
   factory :list do
-    name 'Movies'
+    name { 'Movies' }
     user
   end
 
   factory :tag do
-    name 'Action'
+    name { 'Action' }
     user
   end
 
   factory :user do
     sequence(:email) { |n| "#{n}#{FFaker::Internet.email}" }
-    password FFaker::Internet.password
+    password { FFaker::Internet.password }
   end
 end
