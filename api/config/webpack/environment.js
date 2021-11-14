464a5e36a6faeafe4f562d8bfe2ae20618080556
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2021-11-14 12:56:44 +0100

    Make db port configurable
    And ditch api webpack conf

--- api/config/webpack/environment.js
+++
@@ -1,9 +0,0 @@
-const { environment } = require('@rails/webpacker')
-
-environment.loaders.append('typescript', {
-  test: /\.tsx?$/,
-  exclude: /node_modules/,
-  loader: 'awesome-typescript-loader'
-});
-
-module.exports = environment
