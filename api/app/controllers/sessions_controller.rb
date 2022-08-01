44d04616ff994864776a6cb0483a19e6e68611ae
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2022-08-01 21:38:46 +0200

    Increase JWT token expiration

--- api/app/controllers/sessions_controller.rb
+++ api/app/controllers/sessions_controller.rb
@@ -3,14 +3,16 @@
 class SessionsController < ApplicationController
   skip_before_action :validate_token
 
+  TOKEN_EXPIRATION_DAYS = 7.days
+
   def sign_in
     email = sign_in_params[:email]
     password = sign_in_params[:password]
 
-    user = User.find_by(email: email)
+    user = User.find_by(email:)
     if user&.authenticated?(password)
       token = JwtService.encode(user)
-      cookies.signed[:jwt] = { value: token, httponly: true, expires: 1.day }
+      cookies.signed[:jwt] = { value: token, httponly: true, expires: TOKEN_EXPIRATION_DAYS }
       render status: :ok
     else
       render json: { message: 'Invalid credentials' }, status: 401
